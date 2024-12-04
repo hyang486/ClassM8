a8c0e9e3ad382f757469d92829aeac5f94fa513f
IsaacKim9876 <isaackim4276@gmail.com>
2024-12-04 01:54:49 -0600

    feat: core infrastructure and event management
    Added core infrastructure for the app, including base classes, data models, and repository implementations. Implemented event management features, including UI and business logic for creating and managing events.

---
+++ app/src/main/java/com/cs407/classm8/ui/event/AddEventViewState.kt
@@ -0,0 +1,8 @@
+package com.example.scheduleapp.ui.event
+
+import com.example.scheduleapp.base.ViewEvent
+
+
+sealed class AddEventViewEvent : ViewEvent {
+    data object BackPress : AddEventViewEvent()
+}
