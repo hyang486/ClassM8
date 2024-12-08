e8874d0b305d5ddb4c0b1ae7ebc977a6eb40ba45
Hyeonmin Yang <Didgusals970418@gmail.com>
2024-12-08 02:47:29 -0600

    add element of homeviewstate.kt file, and add logic that can bring 5 future events from current schedule

--- app/src/main/java/com/example/scheduleapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/scheduleapp/ui/home/HomeViewModel.kt
@@ -5,12 +5,28 @@ import com.example.scheduleapp.base.ViewEvent
 import com.example.scheduleapp.usecase.FirebaseLogoutUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import androidx.lifecycle.viewModelScope
+import com.example.scheduleapp.data.model.Event
+import com.example.scheduleapp.usecase.GetUserEventUseCase
+import com.example.scheduleapp.util.DateUtil.getCurrentDateString
+import com.example.scheduleapp.util.DateUtil.isDateLater
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val firebaseLogoutUseCase: FirebaseLogoutUseCase,
+    getUserEventUseCase: GetUserEventUseCase,
 ) : BaseViewModel() {
+
+    init {
+        getUserEventUseCase()
+            .map(::toHomeViewState)
+            .onEach(::onChangedViewState)
+            .launchIn(viewModelScope)
+    }
 
     fun logout() {
         if (firebaseLogoutUseCase()) {
@@ -29,4 +45,11 @@ class HomeViewModel @Inject constructor(
     fun openDrawer() {
         onChangedViewEvent(HomeViewEvent.OpenDrawer)
     }
+
+    private fun toHomeViewState(list: List<Event>): HomeViewState =
+        HomeViewState(
+            currentEvents = list.filter { it.date == getCurrentDateString() },
+            upcomingEvents = list.filter { isDateLater(it.date, getCurrentDateString()) }
+                .sortedBy { it.date }.take(5)
+        )
 }
--- app/src/main/java/com/example/scheduleapp/ui/home/HomeViewState.kt
+++ app/src/main/java/com/example/scheduleapp/ui/home/HomeViewState.kt
@@ -2,6 +2,13 @@ package com.example.scheduleapp.ui.home
 
 import com.example.scheduleapp.base.ViewEvent
 
+import com.example.scheduleapp.base.ViewState
+import com.example.scheduleapp.data.model.Event
+data class HomeViewState(
+    val currentEvents : List<Event> = emptyList(),
+    val upcomingEvents : List<Event> = emptyList(),
+) : ViewState
+
 
 sealed class HomeViewEvent : ViewEvent {
     data object Logout : HomeViewEvent()
