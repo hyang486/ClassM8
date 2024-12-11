96bb46d7b5ea019d51fa69ac7fd751a2206a2791
Sungwon Mun <145941844+piketaco801@users.noreply.github.com>
2024-12-11 00:28:40 -0600

    homepart(if no event or upcoming event it will show no event message and it display upcoming event in mainpage)

--- app/src/main/java/com/example/scheduleapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/scheduleapp/ui/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package com.example.scheduleapp.ui.home
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.scheduleapp.R
@@ -7,6 +10,10 @@ import com.example.scheduleapp.base.BaseFragment
 import com.example.scheduleapp.base.ViewEvent
 import com.example.scheduleapp.base.ViewState
 import com.example.scheduleapp.databinding.FragmentHomeBinding
+import com.example.scheduleapp.ui.adapter.CurrentScheduleAdapter
+import com.example.scheduleapp.ui.adapter.UpcomingEventsAdapter
+import com.example.scheduleapp.ui.event.add.AddEventActivity
+import com.example.scheduleapp.ui.event.update.UpdateEventActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -15,10 +22,27 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override val viewModel by viewModels<HomeViewModel>()
 
-    override fun initUi() {}
+    private val upcomingEventsAdapter = UpcomingEventsAdapter()
+    private val currentScheduleAdapter = CurrentScheduleAdapter { clickItem ->
+        startActivity(UpdateEventActivity.createIntent(context = requireContext(), event = clickItem))
+    }
+    override fun initUi() {
+        binding.rvCurrentEvents.adapter = currentScheduleAdapter
+        binding.rvUpcomingEvents.adapter = upcomingEventsAdapter
+    }
 
     override fun onChangedViewState(state: ViewState) {
+        when(state){
+            is HomeViewState -> {
+                currentScheduleAdapter.submitList(state.currentEvents)
+                upcomingEventsAdapter.submitList(state.upcomingEvents)
+                binding.rvCurrentEvents.isVisible = state.currentEvents.isNotEmpty()
+                binding.emptyCurrentEventsTitle.isVisible = state.currentEvents.isEmpty()
 
+                binding.rvUpcomingEvents.isVisible = state.upcomingEvents.isNotEmpty()
+                binding.emptyUpcommingEventsTitle.isVisible = state.upcomingEvents.isEmpty()
+            }
+        }
     }
 
     override fun onChangeViewEvent(event: ViewEvent) {
@@ -29,7 +53,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
             }
 
             is HomeViewEvent.AddEvent -> {
-                findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToAddEventFragment())
+                startActivity<AddEventActivity>()
             }
 
             is HomeViewEvent.OpenDrawer -> {
@@ -40,6 +64,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
                 binding.dlMain.closeDrawer(binding.drawer)
             }
         }
+    }
+    private inline fun <reified T : AppCompatActivity> startActivity() {
+        startActivity(Intent(requireActivity(), T::class.java))
     }
 }
 
